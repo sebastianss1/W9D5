bc74ce1ddfafb213c85e7573500a4ae3242a449e
Sebastian Sanchez <sebastian@artxcode.io>
2021-04-23 13:53:56 -0400

    finished phase 1

--- src/warmup.js
+++ src/warmup.js
@@ -2,6 +2,13 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
+    const p = document.createElement("p");
+    p.innerText = string; 
+    if (htmlElement.hasChildNodes()) {
+        Array.from(htmlElement.children).forEach((child) => {
+            htmlElement.removeChild(child)}); 
+    };
+    htmlElement.appendChild(p);
 
 };
 
