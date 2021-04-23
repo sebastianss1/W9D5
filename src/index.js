ae3437fd2442d579a9c1e287439231e3536cb410
Robby Balistreri <rbalistreri5@gmail.com>
2021-04-23 15:52:25 -0500

    Complete phase 4

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import warmUp from "./warmup";
 import Clock from "./clock";
 import Dropdown from "./drop_down";
 import Todo from "./todo_list";
+import SlideScroll from "./slide_scroll";
 
--- src/slide_scroll.js
+++ src/slide_scroll.js
@@ -1,34 +1,55 @@
-function debounce(func, wait = 20, immediate = true) {
-  let timeout;
+// function debounce(func, wait = 20, immediate = true) {
+//   let timeout;
+//   console.log('hello')
 
-  // This is the function that is actually executed when
-  // the DOM event is triggered.
-  return function executedFunction() {
-    // Store the context of this and any
-    // parameters passed to executedFunction
-    const context = this;
-    const args = arguments;
+//   // This is the function that is actually executed when
+//   // the DOM event is triggered.
+//   return function executedFunction() {
+//     // Store the context of this and any
+//     // parameters passed to executedFunction
+//     const context = this;
+//     const args = arguments;
 
-    // The function to be called after debounce time has elapsed
-    const later = function () {
-      // null timeout to indicate the debounce ended
-      timeout = null;
+//     // The function to be called after debounce time has elapsed
+//     const later = function () {
+//       // null timeout to indicate the debounce ended
+//       timeout = null;
 
-      // Call function now if you did not in the beginning
-      if (!immediate) func.apply(context, args);
-    };
+//       // Call function now if you did not in the beginning
+//       if (!immediate) func.apply(context, args);
+//     };
 
-    // Determine if you should call the function
-    // on the leading or trail end
-    const callNow = immediate && !timeout;
+//     // Determine if you should call the function
+//     // on the leading or trail end
+//     const callNow = immediate && !timeout;
 
-    // This will reset the waiting every function execution.
-    clearTimeout(timeout);
+//     // This will reset the waiting every function execution.
+//     clearTimeout(timeout);
 
-    // Restart the debounce waiting period - returns true
-    timeout = setTimeout(later, wait);
+//     // Restart the debounce waiting period - returns true
+//     timeout = setTimeout(later, wait);
 
-    // Call immediately if you're doing a leading end execution
-    if (callNow) func.apply(context, args);
-  };
+//     // Call immediately if you're doing a leading end execution
+//     if (callNow) func.apply(context, args);
+//   };
+// }
+
+function debounce() {
+  console.log('hi');
+    const windowHeight = 1080;
+    const images = document.querySelectorAll('img');
+    images.forEach((image) => {
+      const middle = image.height/2;
+      const imagePos = image.getBoundingClientRect()['y'];
+      if (middle + imagePos < windowHeight && middle + imagePos > 0) {
+        image.classList.add('active');
+      } else if (middle + imagePos < 0) {
+        
+        image.classList.remove('active');
+      } else if (middle + imagePos > windowHeight) {
+        image.classList.remove('active');
+      }
+    })
 }
+
+window.addEventListener('scroll', debounce)
